11f5afe36606118fdd9c6dad463dc4578299b01f
Lyuba Pushkina <lyuba.pushkina@gmail.com>
2023-04-25 17:02:04 +0100

    initial code and complete task 1

---
+++ js/01-gallery.js
@@ -0,0 +1,33 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+const galleryList = document.querySelector("ul");
+const markup = galleryItems
+  .map(({ original, preview, description }) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+  })
+  .join("");
+galleryList.insertAdjacentHTML("afterbegin", markup);
+galleryList.addEventListener("click", openFullItem);
+function openFullItem(e) {
+  if (e.target.tagName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img
+      class="gallery__image"
+      src="${e.target.dataset.source}"
+    />
+`);
+  instance.show();
+}
